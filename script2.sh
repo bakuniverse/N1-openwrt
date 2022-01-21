6de49887aaa86ecd718523ddbd476d0056dca65a
bakuniverse <30513244+bakuniverse@users.noreply.github.com>
2022-01-21 12:04:17 +0800

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -2,3 +2,4 @@
 cd openwrt
 sed -i 's/192.168.1.1/192.168.100.252/g' package/base-files/files/bin/config_generate
 sed -i "s/OpenWrt / compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrtt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i 's/5.10/5.15/g' target/linux/x86/Makefile
