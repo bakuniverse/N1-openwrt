5eaebd5718fb1788713222146b9a11e743e5b0b3
bakuniverse <30513244+bakuniverse@users.noreply.github.com>
2022-04-14 11:54:40 +0800

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -3,3 +3,5 @@ cd openwrt
 sed -i 's/192.168.1.1/192.168.100.252/g' package/base-files/files/bin/config_generate
 sed -i "s/OpenWrt / compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrtt /g" package/lean/default-settings/files/zzz-default-settings
 sed -i 's/5.10/5.15/g' target/linux/x86/Makefile
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns package/luci-app-smartdns
+svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns feeds/packages/net/smartdns
