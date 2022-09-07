f8645b98361036593c087e4a219173a11f3b77d9
bakuniverse <30513244+bakuniverse@users.noreply.github.com>
2022-09-07 16:02:49 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,4 +5,4 @@ svn co https://github.com/bakuniverse/openwrt-package/trunk/parted package/parte
 sed -i 's/DEPENDS:=@(.*/DEPENDS:=@(TARGET_bcm27xx||TARGET_bcm53xx||TARGET_ipq40xx||TARGET_ipq806x||TARGET_ipq807x||TARGET_mvebu||TARGET_rockchip||TARGET_armvirt) \\/g' package/lean/autocore/Makefile
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i "s/OpenWrt / compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrtt /g" package/lean/default-settings/files/zzz-default-settings
-sed -i 's/mosdns-neo/mosdns/g' ./feeds/mos/luci-app-mosdns/Makefile
+sed -i 's/mosdns-neo/mosdns/g' feeds/mos/luci-app-mosdns/Makefile
